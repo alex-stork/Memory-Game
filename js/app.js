1db04b2d620d6def07a23bc9eab62e337e0fc454
S. Alex Stork <s.alex.stork@gmail.com>
2019-02-10 21:06:11 -0600

    Improved functionality, not 100%.
    Made some changes, still need to fix a couple discovered bugs, but overall functionality is improved.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 /*
  * Create a list that holds all of your cards
  */
-let defaultCardList = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle"];
+let cardList = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle"];
 
-let cardList = shuffle(defaultCardList);
 
-let tilesFlipped = 0;
-let firstCard = 0;
-let secondCard = 0;
+//Global variables
+const oneCard = document.querySelectorAll(".card"); //represents a single card
+let totalMoves = 0;
+
+//When the player is ready to play a new game, they press on the reset button and the code below will activate
+let newGame = document.querySelector("#restart");
+newGame.addEventListener("click", this.newBoard);
 
 /*
  * Display the cards on the page
@@ -31,8 +34,6 @@ function shuffle(array) {
 	return array;
 }
 
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -44,36 +45,72 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-const oneCard = document.querySelectorAll(".card"); //represents a single card
+function init () {
+	cardList = shuffle(cardList);
+	reShuffle();
+}
 
-function reRoll () {
+function reShuffle() { //reshuffles the deck for next game
 	for (let i = 0; i < oneCard.length; i++) {
 		oneCard[i].classList.add("fa", cardList[i]); //applies randomized list to current deck
 	}
 }
 
-for(let i = 0; i < oneCard.length; i++) {
-	oneCard[i].addEventListener("click", function() {
-		oneCard[i];
-		oneCard[i].classList.add("show", "open");
-		console.log(firstCard);
-		tilesFlipped += 1;
-		console.log("tile flipped = " + tilesFlipped);
-	});
-}
-
 function newBoard() { //resets the board and reshuffles the deck
-	tilesFlipped = 0;
+	cardList = shuffle(cardList);
 	for(let i = 0; i < oneCard.length; i++) {
-		// oneCard[i].classList.remove("show");
-		oneCard[i].classList.remove("fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle", "open");
-		reRoll();
+		oneCard[i].classList.remove("fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle", "open", "match");
 	}
-	cardList = shuffle(defaultCardList);
+	reShuffle();
+	main();
 }
 
-reRoll();
+function main() { //main code
+	let firstCard = "";
+	let secondCard = "";
+	let tilesFlipped = 0;
 
-newGame = document.querySelector("#restart");
+	for (let i = 0; i < oneCard.length; i++) {
+		oneCard[i].addEventListener("click", function () {
+			tilesFlipped++;
+			oneCard[i].classList.add("show", "open");
+			if (tilesFlipped == 1) {
+				firstCard = oneCard[i];
+			}
+			if (tilesFlipped == 2) {
+				secondCard = oneCard[i];
+				compare(firstCard, secondCard);
+			}
+		});
+	}
+}
 
-newGame.addEventListener("click", this.newBoard, false);
+function compare(one, two) {
+	oneString = one.classList[3];
+	twoString = two.classList[3];
+
+	console.log("one is " + oneString + " and two is " + twoString);
+
+	if (oneString == twoString) {
+		one.classList.add("match");
+		two.classList.add("match");
+		console.log("It's a match!");
+		main();
+	} 
+
+	else {
+		console.log("It's not a match.");
+		
+		setTimeout(function() {
+			one.classList.remove("notmatch", "open");
+			two.classList.remove("notmatch", "open");
+	 }, 500)
+
+		one.classList.add("notmatch");
+		two.classList.add("notmatch");
+		main();
+	}
+}
+
+init();
+main();
