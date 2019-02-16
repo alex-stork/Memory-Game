1ceb29e5c32cfbb167c0e99b16dc518eab08b8fb
alex-stork <s.alex.stork@gmail.com>
2019-02-16 13:44:17 -0600

    Redid javascript code - still needs work
    Functionality is much better, no longer buggy when game is reset. Listener events DO NOT in loops, nor do they work as expected if you mistakenly create duplicate listener events like I did in the previous code....
    
    Still need to...
    1) Add animations for matching cards, getting incorrect match, and for winning
    2)  Remove event listener for when the cards you select are matched and flipped over

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 /*
  * Create a list that holds all of your cards
  */
+const oneCard = document.querySelectorAll(".card"); //All cards added to a nodeList
+const newGame = document.querySelector("#restart");
 let cardList = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle"];
+let totalMoves = 0; //Total moves defaulted to 0, increment by 1 for each move
+let tilesMatched = 0; //Total tiles matched defaulted to 0, increment by 2 for each matching pair
+let livesRemaining = 5;
+let firstCard = "";
+let secondCard = "";
+let firstCardElement;
+let secondCardElement;
+let starList = document.getElementsByClassName("fa-star");
 
 
-//Global variables
-const oneCard = document.querySelectorAll(".card"); //represents a single card
-let totalMoves = 0;
-let tilesMatched = 0;
+//Restarts the game!
 
-//When the player is ready to play a new game, they press on the reset button and the code below will activate
-let newGame = document.querySelector("#restart");
 newGame.addEventListener("click", newBoard);
+oneCard[0].addEventListener("click", function() {flipCard(0)});
+oneCard[1].addEventListener("click", function() {flipCard(1)});
+oneCard[2].addEventListener("click", function() {flipCard(2)});
+oneCard[3].addEventListener("click", function() {flipCard(3)});
+oneCard[4].addEventListener("click", function() {flipCard(4)});
+oneCard[5].addEventListener("click", function() {flipCard(5)});
+oneCard[6].addEventListener("click", function() {flipCard(6)});
+oneCard[7].addEventListener("click", function() {flipCard(7)});
+oneCard[8].addEventListener("click", function() {flipCard(8)});
+oneCard[9].addEventListener("click", function() {flipCard(9)});
+oneCard[10].addEventListener("click", function() {flipCard(10)});
+oneCard[11].addEventListener("click", function() {flipCard(11)});
+oneCard[12].addEventListener("click", function() {flipCard(12)});
+oneCard[13].addEventListener("click", function() {flipCard(13)});
+oneCard[14].addEventListener("click", function() {flipCard(14)});
+oneCard[15].addEventListener("click", function() {flipCard(15)});
+
 
 /*
  * Display the cards on the page
@@ -22,102 +44,138 @@ newGame.addEventListener("click", newBoard);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-	while (currentIndex !== 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-	return array;
+    return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//Restarts game, resets all counts, but only when reset button is clicked
+function newBoard() {
+    console.log("Restarting");
+    firstCard = "";
+    secondCard = "";
+    totalMoves = 0;
+    tilesMatched = 0;
+    shuffle(cardList);
 
-function init () {
-	cardList = shuffle(cardList);
-	reShuffle();
+    oneCard.forEach(function (element, index) {
+        oneCard[index].classList.remove("open", "match", "notmatch"); //Need to add "show" to this list when code is complete!
+    })
+
+    document.getElementById("stars").innerHTML = "<li><i class=\"fa fa-star\"></i></li\><li><i class=\"fa fa-star\"></i></li\><li><i class=\"fa fa-star\"></i></li\><li><i class=\"fa fa-star\"></i></li\><li><i class=\"fa fa-star\"></i></li>";
+
+    document.getElementById("card0").removeAttribute("class");
+    document.getElementById("card1").removeAttribute("class");
+    document.getElementById("card2").removeAttribute("class");
+    document.getElementById("card3").removeAttribute("class");
+    document.getElementById("card4").removeAttribute("class");
+    document.getElementById("card5").removeAttribute("class");
+    document.getElementById("card6").removeAttribute("class");
+    document.getElementById("card7").removeAttribute("class");
+    document.getElementById("card8").removeAttribute("class");
+    document.getElementById("card9").removeAttribute("class");
+    document.getElementById("card10").removeAttribute("class");
+    document.getElementById("card11").removeAttribute("class");
+    document.getElementById("card12").removeAttribute("class");
+    document.getElementById("card13").removeAttribute("class");
+    document.getElementById("card14").removeAttribute("class");
+    document.getElementById("card15").removeAttribute("class");
+    
+    main();
 }
 
-function reShuffle() { //reshuffles the deck for next game
-	for (let i = 0; i < oneCard.length; i++) {
-		oneCard[i].classList.add("fa", cardList[i]); //applies randomized list to current deck
-	}
+function setBoard() {
+    console.log("Setting board!");
+    document.getElementById("moves").textContent = 0;
+    document.getElementById("card0").classList.add("fa", cardList[0])
+    document.getElementById("card1").classList.add("fa", cardList[1])
+    document.getElementById("card2").classList.add("fa", cardList[2])
+    document.getElementById("card3").classList.add("fa", cardList[3])
+    document.getElementById("card4").classList.add("fa", cardList[4])
+    document.getElementById("card5").classList.add("fa", cardList[5])
+    document.getElementById("card6").classList.add("fa", cardList[6])
+    document.getElementById("card7").classList.add("fa", cardList[7])
+    document.getElementById("card8").classList.add("fa", cardList[8])
+    document.getElementById("card9").classList.add("fa", cardList[9])
+    document.getElementById("card10").classList.add("fa", cardList[10])
+    document.getElementById("card11").classList.add("fa", cardList[11])
+    document.getElementById("card12").classList.add("fa", cardList[12])
+    document.getElementById("card13").classList.add("fa", cardList[13])
+    document.getElementById("card14").classList.add("fa", cardList[14])
+    document.getElementById("card15").classList.add("fa", cardList[15])
 }
 
-function newBoard() { //resets the board and reshuffles the deck
-	console.log("Resetting game!");
-	tilesMatched = 0;
-	console.log("Tiles matched is now: " + tilesMatched);
-	cardList = shuffle(cardList);
-	oneCard.forEach(function(element, index) {
-		oneCard[index].classList.remove("fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle", "open", "match");
-	})
-	reShuffle();
-	main();
+function showTotalMoves() {
+    totalMoves += 1;
+    document.getElementById("moves").textContent = totalMoves + " ";
 }
 
-function main() { //main code
-	let firstCard = "";
-	let secondCard = "";
-	let tilesFlipped = 0;
-	
-	oneCard.forEach(function(button, index) {
-		console.log(index);
-		oneCard[index].addEventListener("click", function () {
-			tilesFlipped++;
-			oneCard[index].classList.add("show", "open");
-			if (tilesFlipped == 1) {
-				firstCard = oneCard[index];
-			}
-			if (tilesFlipped == 2) {
-				secondCard = oneCard[index];
-				compare(firstCard, secondCard);
-			}
-		});
-	})
+function flipCard(number) {
+    showTotalMoves();
+
+    if (firstCard === "" && secondCard === "") {
+        console.log("FIRST CARD SHOWN");
+        oneCard[number].classList.add("open", "show");
+        firstCard = cardList[number];
+        firstCardElement = oneCard[number];
+    } else if (firstCard !== "" && secondCard === "") {
+        console.log("SECOND CARD SHOWN");
+        oneCard[number].classList.add("open", "show");
+        secondCard = cardList[number];
+        secondCardElement = oneCard[number];
+        if (firstCard === secondCard) {
+            console.log("MATCH"); //Need to actually create an animation here
+            //Keep both cards green, flipped over
+            firstCardElement.classList.add("match");
+            secondCardElement.classList.add("match");
+
+            tilesMatched += 2;
+        } else {
+            console.log("NOT MATCH"); //Need to actually create an animation here
+            //Animation to show not matched, then flip back over
+
+            
+            firstCardElement.classList.add("notmatch");
+            secondCardElement.classList.add("notmatch");
+
+            setTimeout(function () {
+                firstCardElement.classList.remove("notmatch", "open");
+                secondCardElement.classList.remove("notmatch", "open");
+                starList[livesRemaining - 1].classList.remove("fa", "fa-star");
+                livesRemaining -= 1;
+            }, 500)
+        }
+
+        firstCard = "";
+        secondCard = "";
+
+        winValidation();
+    }
 }
 
-function compare(one, two) {
-	oneString = one.classList[3];
-	twoString = two.classList[3];
-
-	console.log("one is " + oneString + " and two is " + twoString);
-
-	if (oneString == twoString) {
-		one.classList.add("match");
-		two.classList.add("match");
-		console.log("It's a match!");
-		tilesMatched += 2;
-		console.log("Tiles matched: " + tilesMatched);
-		main();
-	} 
-
-	else {
-		console.log("It's not a match.");
-		
-		setTimeout(function() {
-			one.classList.remove("notmatch", "open");
-			two.classList.remove("notmatch", "open");
-	 }, 500)
-
-		one.classList.add("notmatch");
-		two.classList.add("notmatch");
-		main();
-	}
+function main() {
+    shuffle(cardList);
+    setBoard();
 }
 
-init();
+function winValidation() {
+    if(tilesMatched === 16) {
+        setTimeout(function () {
+            alert("YOU WIN!!! YOU WON IN " + totalMoves + " moves!");
+        }, 500)
+    }
+         else {
+        console.log("You have matched " + tilesMatched + " tiles");
+    }
+}
+
 main();
